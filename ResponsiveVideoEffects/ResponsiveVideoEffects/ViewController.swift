33a65035a406ebc7aad3d1a4c30fc4fcc4364ab6
Keith Moon <keef@keefmoon.com>
2017-05-04 01:21:15 +0100

    Initial commit of project testing video effects

---
+++ ResponsiveVideoEffects/ResponsiveVideoEffects/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  ResponsiveVideoEffects
+//
+//  Created by Keith Moon on 03/05/2017.
+//  Copyright © 2017 Keith Moon. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var export: AVAssetExportSession?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let now = Date()
+        
+        let videoURL = Bundle.main.url(forResource: "Video1", withExtension: "mov", subdirectory: "Videos")!
+        let videoAsset = AVAsset(url: videoURL)
+        
+        
+//        let filter = CIFilter(name: "CIVibrance")!
+        let composition = AVVideoComposition(asset: videoAsset, applyingCIFiltersWithHandler: { request in
+            
+            
+            // Clamp to avoid effecting transparent pixels at the image edges
+            var source = request.sourceImage.clampingToExtent()
+            
+            source = source.applyingFilter("CIVibrance", withInputParameters: ["inputAmount": 8.0])
+//            source = source.applyingFilter("CIGaussianBlur", withInputParameters: [kCIInputRadiusKey: 100])
+            request.finish(with: source, context: nil)
+            
+            
+//            filter.setValue(source, forKey: kCIInputImageKey)
+//            
+//            // Amount
+//            filter.setValue(NSNumber(value: 5.0), forKey: "inputAmount")
+//            
+//            // Crop the effected output to the bounds of the original image
+//            let output = filter.outputImage!.cropping(to: request.sourceImage.extent)
+//            
+//            // Provide the filter output to the composition
+//            request.finish(with: output, context: nil)
+        })
+        
+        let docsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let docsURL = URL(fileURLWithPath: docsPath)
+        let outputURL = docsURL.appendingPathComponent("video1.mov", isDirectory: false)
+        
+        
+        let export = AVAssetExportSession(asset: videoAsset, presetName: AVAssetExportPreset1280x720)!
+        export.outputFileType = AVFileTypeQuickTimeMovie
+        export.outputURL = outputURL
+        export.videoComposition = composition
+        
+        export.exportAsynchronously {
+            
+            let timeTaken = now.timeIntervalSinceNow
+            print("Time taken: \(timeTaken)")
+            
+            switch export.status {
+                
+            case .completed:
+                print("Complete")
+                
+            case .cancelled:
+                print("Cancelled")
+                
+            case .exporting:
+                print("Exporting")
+                
+            case .failed:
+                print("Failed: \(export.error!))")
+                
+            case .waiting:
+                print("Waiting")
+                
+            case .unknown:
+                print("Unknown")
+                
+            }
+            
+        }
+        
+        export.addObserver(self, forKeyPath: "progress", options: .new, context: nil)
+        
+        self.export = export
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        guard let progress = object as? NSNumber else { return }
+        
+        print("Progress: \(progress)")
+        
+    }
+    
+    deinit {
+        self.export?.removeObserver(self, forKeyPath: "progress")
+    }
+    
+}
+
